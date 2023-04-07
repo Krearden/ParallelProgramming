cc5de48785b1b9203a85039bd40255fc5fcc8b27
krearden <krearden@yandex.ru>
2023-04-07 19:13:30 +0500

    just for bakcup

--- LR2/LR2_Parallel/LR2_Parallel/LR2_Parallel.cpp
+++ LR2/LR2_Parallel/LR2_Parallel/LR2_Parallel.cpp
@@ -67,6 +67,50 @@ int main(int argc, char** argv)
 		partial_image.data, partial_rows * partial_cols * channels, MPI_BYTE,
 		0, MPI_COMM_WORLD);
 
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	//полоска части изобаржения сверху высоты PADDING
+	Rect top_strip(0, 0, partial_cols, 3);
+	Mat top_strip_image = partial_image(top_strip);
+	
+	//полоска части изобаржения снизу высоты PADDING
+	Rect bottom_strip(0, partial_rows - 3, partial_cols, 3);
+	Mat bottom_strip_image = partial_image(bottom_strip);
+
+	//размер полосок изображения в байтах
+	int strip_size = top_strip_image.total()* top_strip_image.elemSize();
+
+
+	if (rank == 0)
+	{
+		//send to 1
+		int proccess_to_communicate = 1;
+		// Отправить нижнюю полоску 1му процессу с помощью функции MPI_Send
+		MPI_Send(bottom_strip_image.data, strip_size, MPI_BYTE, proccess_to_communicate, 0, MPI_COMM_WORLD);
+		
+		//recieve from 1
+		// Выделить память для получения полоски
+		Mat received_top_strip_image_of_proccess_one(3, partial_cols, CV_8UC3);
+		// Принять полоску с помощью функции MPI_Recv
+		MPI_Recv(received_top_strip_image_of_proccess_one.data, strip_size, MPI_BYTE, proccess_to_communicate, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		imshow("received_top_strip_image_of_proccess_one", received_top_strip_image_of_proccess_one);
+		waitKey(0);
+		
+	}
+	else if (rank == (procs_amount - 1))
+	{
+		//send to (rank - 1)
+		//send to (rank + 1)
+
+		//recieve from (rank - 1)
+		//revieve from (rank + 1)
+	}
+	else
+	{
+		//send to (rank - 1)
+		//recieve from (rank - 1)
+	}
+
 	imshow("Partial img", partial_image);
 	waitKey(0);
 
