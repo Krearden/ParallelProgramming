9f82e7aee199bb5e97714ee234e9d47335a0f40b
krearden <krearden@yandex.ru>
2023-04-17 18:45:20 +0500

    backup

--- LR2/LR2_Parallel/LR2_Parallel/LR2_Parallel.cpp
+++ LR2/LR2_Parallel/LR2_Parallel/LR2_Parallel.cpp
@@ -1,4 +1,4 @@
-﻿//Лабораторная работа 2, вариант 21
+﻿//Лабораторная работа 2, вариант 21 - часть с параллельной программной
 //Выполнил студент 3-го курса физического факультета Запорожченко Кирилл (ФЗ-11)
 //2023 г.
 
@@ -23,9 +23,10 @@ int main(int argc, char** argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &procs_amount);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	int partial_rows, partial_cols;
+	int partial_rows, partial_cols, input_cols, input_rows;
 	int channels;
 	Mat temp_image;
+	
 
 	if (rank == 0)
 	{
@@ -38,6 +39,9 @@ int main(int argc, char** argv)
 			MPI_Finalize();
 			return 1;
 		}
+
+		input_cols = input_image.cols;
+		input_rows = input_image.rows;
 
 		//создаем временное изображение с добавленным паддингом 
 		temp_image = Mat::zeros(input_image.size(), input_image.type());
@@ -58,9 +62,12 @@ int main(int argc, char** argv)
 	MPI_Bcast(&partial_rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&partial_cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&channels, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&input_cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&input_rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	//выделяем память для части изображения на каждом процессе
 	Mat partial_image(partial_rows, partial_cols, CV_8UC3);
+	Mat partial_image_with_padding;
 
 	//делим изображение между процессами
 	MPI_Scatter(temp_image.data, partial_rows * partial_cols * channels, MPI_BYTE,
@@ -78,9 +85,10 @@ int main(int argc, char** argv)
 	Mat bottom_strip_image = partial_image(bottom_strip);
 
 	//размер полосок изображения в байтах
-	int strip_size = top_strip_image.total()* top_strip_image.elemSize();
+	int strip_size = top_strip_image.total() * top_strip_image.elemSize();
 
 
+	//если нулевой процесс
 	if (rank == 0)
 	{
 		//send to 1
@@ -90,28 +98,133 @@ int main(int argc, char** argv)
 		
 		//recieve from 1
 		// Выделить память для получения полоски
-		Mat received_top_strip_image_of_proccess_one(3, partial_cols, CV_8UC3);
+		Mat received_strip_for_bottom_padding(3, partial_cols, CV_8UC3);
+
 		// Принять полоску с помощью функции MPI_Recv
-		MPI_Recv(received_top_strip_image_of_proccess_one.data, strip_size, MPI_BYTE, proccess_to_communicate, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		imshow("received_top_strip_image_of_proccess_one", received_top_strip_image_of_proccess_one);
-		waitKey(0);
+		MPI_Recv(received_strip_for_bottom_padding.data, strip_size, MPI_BYTE, proccess_to_communicate, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		
+		//объединяем
+		vconcat(partial_image, received_strip_for_bottom_padding, partial_image_with_padding);
 	}
+	//если последний процесс
 	else if (rank == (procs_amount - 1))
 	{
+		int proccess_to_communicate = (rank - 1);
+
 		//send to (rank - 1)
-		//send to (rank + 1)
+		MPI_Send(top_strip_image.data, strip_size, MPI_BYTE, proccess_to_communicate, 0, MPI_COMM_WORLD);
 
 		//recieve from (rank - 1)
-		//revieve from (rank + 1)
+		Mat received_strip_for_top_padding(3, partial_cols, CV_8UC3);
+		MPI_Recv(received_strip_for_top_padding.data, strip_size, MPI_BYTE, proccess_to_communicate, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		//объединяем
+		vconcat(received_strip_for_top_padding, partial_image, partial_image_with_padding);
 	}
+	//между 0 и последним
 	else
 	{
-		//send to (rank - 1)
+		int rank_minus_one = (rank - 1);
+		int rank_plus_one = (rank + 1);
+
+		//send to (rank - 1) top strip img
+		MPI_Send(top_strip_image.data, strip_size, MPI_BYTE, rank_minus_one, 0, MPI_COMM_WORLD);
+
+		//send to (rank + 1) bottom strip img
+		MPI_Send(bottom_strip_image.data, strip_size, MPI_BYTE, rank_plus_one, 0, MPI_COMM_WORLD);
+
 		//recieve from (rank - 1)
+		Mat received_strip_for_top_padding(3, partial_cols, CV_8UC3);
+		MPI_Recv(received_strip_for_top_padding.data, strip_size, MPI_BYTE, rank_minus_one, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		
+		//revieve from (rank + 1)
+		Mat received_strip_for_bottom_padding(3, partial_cols, CV_8UC3);
+		MPI_Recv(received_strip_for_bottom_padding.data, strip_size, MPI_BYTE, rank_plus_one, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		//объеденяем полученные полоски размера PADDING и часть изображения
+		vconcat(received_strip_for_top_padding, partial_image, partial_image_with_padding);
+		vconcat(partial_image_with_padding, received_strip_for_bottom_padding, partial_image_with_padding);
 	}
 
-	imshow("Partial img", partial_image);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	//Теперь у каждого процесса есть своя часть изображения с добавленным padding, можно обрабатывать
+	
+	// Создаем матрицу наращивания 7x7
+	int extensionMatix[7][7] = {
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+		{1, 1, 1, 1, 1, 1, 1},
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+	};
+
+	//создаем новое изображение 
+	int partial_output_cols = partial_image_with_padding.cols - PADDING * 2;
+	int partial_output_rows = partial_image_with_padding.rows - PADDING * 2;
+	Mat partial_output_image = Mat::zeros(partial_output_rows, partial_output_cols, partial_image_with_padding.type());
+
+	//цикл для применения матричного фильтра
+	//проходимся по всем пикселям изображения, кроме padding
+	Mat pixel_neighborhood;
+	Vec3b temp_pixel(0, 0, 0);
+	int r = 0, g = 0, b = 0;
+
+	for (int i = PADDING; i < partial_image_with_padding.rows - PADDING; i++)
+	{
+		for (int j = PADDING; j < partial_image_with_padding.cols - PADDING; j++)
+		{
+			// Выбираем диапазон столбцов и строк в матрице изображения
+			pixel_neighborhood = partial_image_with_padding.rowRange(i - PADDING, i + PADDING + 1)
+				.colRange(j - PADDING, j + PADDING + 1);
+
+			//выполняем операцию свертки
+			for (int k = 0; k < 7; k++)
+			{
+				for (int l = 0; l < 7; l++)
+				{
+					b += pixel_neighborhood.at<Vec3b>(k, l)[0] * extensionMatix[k][l];
+					g += pixel_neighborhood.at<Vec3b>(k, l)[1] * extensionMatix[k][l];
+					r += pixel_neighborhood.at<Vec3b>(k, l)[2] * extensionMatix[k][l];
+				}
+			}
+
+			//нормируем
+			r /= NORM_COEFICIENT;
+			g /= NORM_COEFICIENT;
+			b /= NORM_COEFICIENT;
+
+			//присваеваем значения каналов пикселю
+			temp_pixel[0] = b;
+			temp_pixel[1] = g;
+			temp_pixel[2] = r;
+
+			//записываем найденный пиксель в новое изображение
+			partial_output_image.at<Vec3b>(i - PADDING, j - PADDING) = temp_pixel;
+
+			//обнуляем значения для следующей итерации
+			temp_pixel = Vec3b(0, 0, 0);
+			b = 0; g = 0; r = 0;
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	//Mat output_image;
+
+	//if (rank == 0) {
+	//	// выделяем память для массива на корневом процессе
+	//	output_image = Mat::zeros(input_rows, input_cols, CV_8UC3);
+	//}
+	//MPI_Barrier(MPI_COMM_WORLD);
+	//MPI_Gather(partial_output_image.data, partial_output_image.cols* partial_output_image.rows* channels, MPI_BYTE,
+	//	output_image.data, output_image.rows* output_image.cols* channels, MPI_BYTE, 0, MPI_COMM_WORLD);
+
+
+	imshow("Processed img no padding", partial_output_image);
+	//imshow("Output", output_image);
 	waitKey(0);
 
 	printf("\ntotal procs = %d, i'm number %d; pr = %d, pc = %d, ch = %d, img = %d", procs_amount, rank, partial_rows, partial_cols, channels, partial_image.at<Vec3b>(0, 0)[0]);
